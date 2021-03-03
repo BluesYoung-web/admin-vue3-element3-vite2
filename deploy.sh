8d115dc87fea1e93e8aceda1ddbabacce9261c4f
张扬 <15171255945@163.com>
2021-03-03 17:28:37 +0800

    测试

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,16 @@
 #! usr/bin/env sh
+###
+ # @Author: zhangyang
+ # @Date: 2021-03-03 16:23:04
+ # @LastEditTime: 2021-03-03 17:26:56
+ # @Description: 
+### 
 # 发生错误时，终止脚本
 set -e
 # 进入构建目录
 cd dist
+git config --gloabl user.eamil "15171255945@163.com"
+git config --gloabl user.name "BluesYoung-web"
 git init
 git add -A
 git commit -m "deploy"
