b740262ac7863fb93b8cdbe8f917d51943837e10
张扬 <15171255945@163.com>
2021-03-03 16:24:59 +0800

    持续集成

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#! usr/bin/env sh
+# 发生错误时，终止脚本
+set -e
+# 进入构建目录
+cd dist
+git init
+git add -A
+git commit -m "deploy"
+# 推送到指定目录
+git push -f https://443b8baebb8a012ffb24c59c536ef79a@gitee.com:BluseYoung-web/admin-management.git master
+# 返回上层目录
+cd -
