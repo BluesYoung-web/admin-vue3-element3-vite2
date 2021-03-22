2ca918878a31ba35b84ae49449268343d07d1985
张扬 <15171255945@163.com>
2021-03-22 15:23:34 +0800

    fix: :bug: 修复鉴权异常

--- src/permission.ts
+++ src/permission.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: zhangyang
  * @Date: 2020-12-09 17:21:19
- * @LastEditTime: 2021-03-10 11:45:59
+ * @LastEditTime: 2021-03-22 15:10:07
  * @Description: 页面权限控制
  */
 import router from './route/index';
@@ -33,7 +33,7 @@ router.beforeEach(async (to, from, next) => {
   document.title = (to.meta.title as string) || '小黑后台';
 
   // 此处添加鉴权
-  if (!getToken().token && !getCommonRoutes().includes(to.path)) {
+  if (!getToken().token && (!getCommonRoutes().includes(to.path) || to.path === '/Dashboard/dashboard')) {
     await ElMessageBox.confirm('请先去登录', '提示', {
       type: 'error',
       showCancelButton: false,
