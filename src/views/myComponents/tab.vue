2b3d72f7fcbb03e1254adbf6c5523f8cd6faf9b2
张扬 <15171255945@163.com>
2021-07-02 14:41:33 +0800

    feat: :sparkles: JSON 静态分离

--- src/views/myComponents/tab.vue
+++ src/views/myComponents/tab.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: zhangyang
  * @Date: 2021-03-25 11:46:31
- * @LastEditTime: 2021-07-02 11:08:00
+ * @LastEditTime: 2021-07-02 14:36:55
  * @Description: tab 组件
 -->
 <template>
@@ -27,6 +27,12 @@
         <h3 v-for="(item, index) in Object.entries(cssVar)" :key="index" :style="{ backgroundColor: item[1] }">{{ item[0] }}</h3>
       </div>
     </template>
+    <template #t6>
+      <div>
+        <h1>json 静态分离</h1>
+        <p>{{ data }}</p>
+      </div>
+    </template>
   </young-tabs>
 </template>
 
@@ -34,12 +40,19 @@
 import { ref } from 'vue';
 import YoungContextMenu from '/components/YoungContextMenu/index.vue';
 import cssVar from '/src/styles/variables.module.scss';
+
+const data = ref();
+fetch('/json/test.json')
+  .then((res) => res.json())
+  .then((dt) => data.value = dt);
+
 const tabsMenu = ref<TabItem[]>([
   { label: 'tab1', name: 't1' },
   { label: 'tab2', name: 't2' },
   { label: 'tab3', name: 't3' },
   { label: 'tab4', name: 't4' },
   { label: 'tab5', name: 't5' },
+  { label: 'tab6', name: 't6' }
 ]);
 const activeTab = ref('t1');
 const color = ref('red');
