7e82f100ca27837eda90d57d7a826133a1da6e1e
Blues Young <15171255945@163.com>
2021-09-10 17:39:43 +0800

    fix: :bug: 使用 useLocalStorage 实现全局刷新之后恢复上次的侧边栏导航

--- src/layout/components/HeadNav/index.vue
+++ src/layout/components/HeadNav/index.vue
@@ -1,31 +1,34 @@
 <!--
  * @Author: zhangyang
  * @Date: 2020-12-10 10:10:36
- * @LastEditTime: 2021-08-27 09:34:18
+ * @LastEditTime: 2021-09-10 09:07:48
  * @Description: 顶部导航(一级节点)
 -->
 <template>
-  <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+  <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="(key) => activeIndex=key">
     <el-menu-item v-for="(route, index) in routes" :key="index" :item="route" :index="route.node_name" v-text="route.node_name" />
   </el-menu>
 </template>
 <script lang="ts" setup>
-import { getNavArr } from '../../../store/sessionStorage/index';
+import { NAV_ARR } from '../../../store/sessionStorage/index';
 import { useNav } from '../../../store';
+import { useLocalStorage } from '@vueuse/core';
+
+const navKey: symbol = Symbol('__顶部导航名称__');
 
 // 获取顶部导航栏数据(一级节点)
-const routes = computed(() => getNavArr().filter((item) => item.is_show === 1));
+const routes = computed(() => NAV_ARR.value.filter((item) => item.is_show === 1));
 // 设置对应的侧边导航栏数据(二级节点及以后)
 const { setLeftArr } = useNav();
 setLeftArr(routes.value[0]?.part ?? []);
 // 初始激活标签
-const activeIndex = ref(routes.value[0]?.node_name ?? '');
+const activeIndex = useLocalStorage(navKey.toString(), routes.value[0]?.node_name ?? '');
 
-const handleSelect = (key: string) => {
+watchEffect(() => {
   routes.value?.forEach((item) => {
-    item.node_name === key && setLeftArr(item.part);
+    item.node_name === activeIndex.value && setLeftArr(item.part);
   });
-}
+});
 </script>
 
 <style scoped>
